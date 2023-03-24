06b8e282e335a6c9d5dc58a35dec0b40145ed863
DevTinel <davidaugustolol36@gmail.com>
2023-03-23 21:51:04 -0300

    crew efeitos no select e borda na selecao da pagina

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/shared/logo.svg";
 import { Link } from "react-router-dom";
 
@@ -6,24 +6,56 @@ import styles from "./Header.module.css";
 import "../../App.css";
 
 export const Header = () => {
+  const [selected, setSelected] = useState(1);
+  const handleClickHeader = (id) => {
+    if (selected === id) {
+      setSelected(null);
+    } else {
+      setSelected(id);
+    }
+  };
   return (
     <header>
       <nav className={styles.nav}>
         <img src={Logo} alt="Logo" />
         <div className={styles.navLinks}>
-          <Link to="/" className={`${styles.navLinksItem}`}>
+          <Link
+            to="/"
+            onClick={() => handleClickHeader(1)}
+            className={`${styles.navLinksItem} ${
+              selected === 1 ? styles.selected : ""
+            }`}
+          >
             <span>00</span>
             <p>HOME</p>
           </Link>
-          <Link to="/destination" className={styles.navLinksItem}>
+          <Link
+            to="/destination"
+            onClick={() => handleClickHeader(2)}
+            className={`${styles.navLinksItem} ${
+              selected === 2 ? styles.selected : ""
+            }`}
+          >
             <span>01</span>
             <p>DESTINATION</p>
           </Link>
-          <Link to="/crew" className={styles.navLinksItem}>
+          <Link
+            to="/crew"
+            onClick={() => handleClickHeader(3)}
+            className={`${styles.navLinksItem} ${
+              selected === 3 ? styles.selected : ""
+            }`}
+          >
             <span>02</span>
             <p>CREW</p>
           </Link>
-          <Link to="/technology" className={styles.navLinksItem}>
+          <Link
+            to="/technology"
+            onClick={() => handleClickHeader(4)}
+            className={`${styles.navLinksItem} ${
+              selected === 4 ? styles.selected : ""
+            }`}
+          >
             <span>03</span>
             <p>TECHNOLOGY</p>
           </Link>
